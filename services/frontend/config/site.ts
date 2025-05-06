1c9b26e9d7581a9916ca492e63a797edbff0e0a3
Justin Neubert <neubert_justin@outlook.com>
2025-05-06 20:59:51 +0200

    chore: Bump version to 1.0.0-beta7 in backend and frontend configurations

--- services/frontend/config/site.ts
+++ services/frontend/config/site.ts
@@ -3,7 +3,7 @@ export type SiteConfig = typeof siteConfig;
 export const siteConfig = {
   name: "exFlow",
   description: "exFlow is an workflow automation tool",
-  version: "1.0.0-beta6",
+  version: "1.0.0-beta7",
   navItems: [
     {
       label: "Dashboard",
