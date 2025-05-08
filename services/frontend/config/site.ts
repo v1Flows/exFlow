11363b013e49df3592297138a4ee33f83ef3e253
Justin Neubert <neubert_justin@outlook.com>
2025-05-08 16:21:45 +0200

    chore: update version to 1.0.0-beta9 in backend and frontend configurations

--- services/frontend/config/site.ts
+++ services/frontend/config/site.ts
@@ -3,7 +3,7 @@ export type SiteConfig = typeof siteConfig;
 export const siteConfig = {
   name: "exFlow",
   description: "exFlow is an workflow automation tool",
-  version: "1.0.0-beta8",
+  version: "1.0.0-beta9",
   navItems: [
     {
       label: "Dashboard",
