f1a0d93e5f8b1a90435eaed322000f4b7ac0092a
Justin Neubert <neubert_justin@outlook.com>
2025-05-15 15:23:22 +0200

    feat: Add error handling and conditional rendering for admin users page

--- services/frontend/app/admin/users/page.tsx
+++ services/frontend/app/admin/users/page.tsx
@@ -3,6 +3,7 @@ import { Divider } from "@heroui/react";
 import AdminUsersHeading from "@/components/admin/users/heading";
 import AdminGetUsers from "@/lib/fetch/admin/users";
 import { AdminUsersList } from "@/components/admin/users/list";
+import ErrorCard from "@/components/error/ErrorCard";
 
 export default async function AdminUsersPage() {
   const usersData = AdminGetUsers();
@@ -11,9 +12,15 @@ export default async function AdminUsersPage() {
 
   return (
     <main>
-      <AdminUsersHeading />
-      <Divider className="mt-4 mb-4" />
-      <AdminUsersList users={users.data.users} />
+      {users.success ? (
+        <>
+          <AdminUsersHeading />
+          <Divider className="mt-4 mb-4" />
+          <AdminUsersList users={users.data.users} />
+        </>
+      ) : (
+        <ErrorCard error={users.error} message={users.message} />
+      )}
     </main>
   );
 }
