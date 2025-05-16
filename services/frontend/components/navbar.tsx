346f7638337d8c3e1c106b0083c43468f705e0d2
Justin Neubert <neubert_justin@outlook.com>
2025-05-16 22:11:14 +0200

    feat: Refactor admin dropdown in Navbar for improved accessibility and organization

--- services/frontend/components/navbar.tsx
+++ services/frontend/components/navbar.tsx
@@ -29,7 +29,6 @@ import { usePathname, useRouter } from "next/navigation";
 import { useState } from "react";
 
 import { siteConfig } from "@/config/site";
-import { ThemeSwitch } from "@/components/theme-switch";
 import { SearchIcon } from "@/components/icons";
 import { Logout } from "@/lib/logout";
 
@@ -122,127 +121,129 @@ export const Navbar = ({ userDetails, session, settings }) => {
             </NavbarItem>
           ))}
         </ul>
-        {userDetails.role === "admin" && (
-          <Dropdown>
-            <NavbarItem>
-              <DropdownTrigger>
-                <Button
-                  disableRipple
-                  className="p-0 bg-transparent data-[hover=true]:bg-transparent font-bold"
-                  color="danger"
-                  endContent={<Icon icon="hugeicons:arrow-down-01" />}
-                  radius="sm"
-                  variant="light"
+        <NavbarItem className="hidden lg:flex">
+          {userDetails.role === "admin" && (
+            <Dropdown>
+              <NavbarItem>
+                <DropdownTrigger>
+                  <Button
+                    disableRipple
+                    className="p-0 bg-transparent data-[hover=true]:bg-transparent font-bold"
+                    color="danger"
+                    endContent={<Icon icon="hugeicons:arrow-down-01" />}
+                    radius="sm"
+                    variant="light"
+                  >
+                    Admin
+                  </Button>
+                </DropdownTrigger>
+              </NavbarItem>
+              <DropdownMenu
+                aria-label="Admin Actions"
+                itemClasses={{
+                  base: "gap-4",
+                }}
+              >
+                <DropdownItem
+                  key="projects"
+                  description="Manage all projects"
+                  startContent={
+                    <Icon
+                      className="text-danger"
+                      icon="hugeicons:ai-folder-01"
+                      width={24}
+                    />
+                  }
+                  onPress={() => {
+                    router.push("/admin/projects");
+                  }}
                 >
-                  Admin
-                </Button>
-              </DropdownTrigger>
-            </NavbarItem>
-            <DropdownMenu
-              aria-label="Admin Actions"
-              itemClasses={{
-                base: "gap-4",
-              }}
-            >
-              <DropdownItem
-                key="projects"
-                description="Manage all projects"
-                startContent={
-                  <Icon
-                    className="text-danger"
-                    icon="hugeicons:ai-folder-01"
-                    width={24}
-                  />
-                }
-                onPress={() => {
-                  router.push("/admin/projects");
-                }}
-              >
-                Projects
-              </DropdownItem>
-              <DropdownItem
-                key="flows"
-                description="Manage all flows & folders"
-                startContent={
-                  <Icon
-                    className="text-danger"
-                    icon="hugeicons:workflow-square-10"
-                    width={24}
-                  />
-                }
-                onPress={() => {
-                  router.push("/admin/flows");
-                }}
-              >
-                Flows
-              </DropdownItem>
-              <DropdownItem
-                key="executions"
-                description="Manage all executions"
-                startContent={
-                  <Icon
-                    className="text-danger"
-                    icon="hugeicons:rocket-02"
-                    width={24}
-                  />
-                }
-                onPress={() => {
-                  router.push("/admin/executions");
-                }}
-              >
-                Executions
-              </DropdownItem>
-              <DropdownItem
-                key="runners"
-                description="Manage all runners"
-                startContent={
-                  <Icon
-                    className="text-danger"
-                    icon="hugeicons:ai-brain-04"
-                    width={24}
-                  />
-                }
-                onPress={() => {
-                  router.push("/admin/runners");
-                }}
-              >
-                Runners
-              </DropdownItem>
-              <DropdownItem
-                key="users"
-                description="Manage all users"
-                startContent={
-                  <Icon
-                    className="text-danger"
-                    icon="hugeicons:location-user-02"
-                    width={24}
-                  />
-                }
-                onPress={() => {
-                  router.push("/admin/users");
-                }}
-              >
-                Users
-              </DropdownItem>
-              <DropdownItem
-                key="page_settings"
-                description="Manage the page settings"
-                startContent={
-                  <Icon
-                    className="text-danger"
-                    icon="hugeicons:settings-05"
-                    width={24}
-                  />
-                }
-                onPress={() => {
-                  router.push("/admin/page-settings");
-                }}
-              >
-                Page Settings
-              </DropdownItem>
-            </DropdownMenu>
-          </Dropdown>
-        )}
+                  Projects
+                </DropdownItem>
+                <DropdownItem
+                  key="flows"
+                  description="Manage all flows & folders"
+                  startContent={
+                    <Icon
+                      className="text-danger"
+                      icon="hugeicons:workflow-square-10"
+                      width={24}
+                    />
+                  }
+                  onPress={() => {
+                    router.push("/admin/flows");
+                  }}
+                >
+                  Flows
+                </DropdownItem>
+                <DropdownItem
+                  key="executions"
+                  description="Manage all executions"
+                  startContent={
+                    <Icon
+                      className="text-danger"
+                      icon="hugeicons:rocket-02"
+                      width={24}
+                    />
+                  }
+                  onPress={() => {
+                    router.push("/admin/executions");
+                  }}
+                >
+                  Executions
+                </DropdownItem>
+                <DropdownItem
+                  key="runners"
+                  description="Manage all runners"
+                  startContent={
+                    <Icon
+                      className="text-danger"
+                      icon="hugeicons:ai-brain-04"
+                      width={24}
+                    />
+                  }
+                  onPress={() => {
+                    router.push("/admin/runners");
+                  }}
+                >
+                  Runners
+                </DropdownItem>
+                <DropdownItem
+                  key="users"
+                  description="Manage all users"
+                  startContent={
+                    <Icon
+                      className="text-danger"
+                      icon="hugeicons:location-user-02"
+                      width={24}
+                    />
+                  }
+                  onPress={() => {
+                    router.push("/admin/users");
+                  }}
+                >
+                  Users
+                </DropdownItem>
+                <DropdownItem
+                  key="page_settings"
+                  description="Manage the page settings"
+                  startContent={
+                    <Icon
+                      className="text-danger"
+                      icon="hugeicons:settings-05"
+                      width={24}
+                    />
+                  }
+                  onPress={() => {
+                    router.push("/admin/page-settings");
+                  }}
+                >
+                  Page Settings
+                </DropdownItem>
+              </DropdownMenu>
+            </Dropdown>
+          )}
+        </NavbarItem>
       </NavbarContent>
 
       <NavbarContent
@@ -331,7 +332,75 @@ export const Navbar = ({ userDetails, session, settings }) => {
       </NavbarContent>
 
       <NavbarContent className="sm:hidden basis-1 pl-4" justify="end">
-        <ThemeSwitch />
+        <Dropdown placement="bottom-end">
+          <DropdownTrigger>
+            <Avatar
+              isBordered
+              as="button"
+              className="transition-transform"
+              color="primary"
+              name={userDetails.username}
+              size="sm"
+            />
+          </DropdownTrigger>
+          <DropdownMenu aria-label="Profile Actions" variant="flat">
+            <DropdownItem key="profile" className="h-14 gap-2">
+              <p className="font-semibold">Signed in as</p>
+              <p className="font-semibold">
+                {userDetails.username}
+                {" | "}
+                {userDetails.role === "admin" && (
+                  <span className="text-danger font-bold">Admin</span>
+                )}
+              </p>
+            </DropdownItem>
+            <DropdownItem
+              key="settings"
+              showDivider
+              startContent={
+                <Icon icon="hugeicons:user-id-verification" width={20} />
+              }
+              onPress={() => {
+                router.push("/profile");
+              }}
+            >
+              View Profile
+            </DropdownItem>
+            {theme === "light" ? (
+              <DropdownItem
+                key="dark_mode"
+                startContent={<Icon icon="hugeicons:moon-01" width={20} />}
+                onPress={onChange}
+              >
+                Dark Mode
+              </DropdownItem>
+            ) : (
+              <DropdownItem
+                key="white_mode"
+                startContent={<Icon icon="hugeicons:sun-01" width={20} />}
+                onPress={onChange}
+              >
+                White Mode
+              </DropdownItem>
+            )}
+            <DropdownItem
+              key="token"
+              showDivider
+              startContent={<Icon icon="hugeicons:key-02" width={20} />}
+              onPress={copyToken}
+            >
+              Copy Token
+            </DropdownItem>
+            <DropdownItem
+              key="logout"
+              color="danger"
+              startContent={<Icon icon="hugeicons:logout-02" width={20} />}
+              onPress={LogoutHandler}
+            >
+              Log Out
+            </DropdownItem>
+          </DropdownMenu>
+        </Dropdown>
         <NavbarMenuToggle />
       </NavbarContent>
 
@@ -360,6 +429,127 @@ export const Navbar = ({ userDetails, session, settings }) => {
               </NextLink>
             </NavbarMenuItem>
           ))}
+          {userDetails.role === "admin" && (
+            <Dropdown>
+              <NavbarItem>
+                <DropdownTrigger>
+                  <Button
+                    disableRipple
+                    className="p-0 bg-transparent data-[hover=true]:bg-transparent font-bold"
+                    color="danger"
+                    endContent={<Icon icon="hugeicons:arrow-down-01" />}
+                    radius="sm"
+                    variant="light"
+                  >
+                    Admin
+                  </Button>
+                </DropdownTrigger>
+              </NavbarItem>
+              <DropdownMenu
+                aria-label="Admin Actions"
+                itemClasses={{
+                  base: "gap-4",
+                }}
+              >
+                <DropdownItem
+                  key="projects"
+                  description="Manage all projects"
+                  startContent={
+                    <Icon
+                      className="text-danger"
+                      icon="hugeicons:ai-folder-01"
+                      width={24}
+                    />
+                  }
+                  onPress={() => {
+                    router.push("/admin/projects");
+                  }}
+                >
+                  Projects
+                </DropdownItem>
+                <DropdownItem
+                  key="flows"
+                  description="Manage all flows & folders"
+                  startContent={
+                    <Icon
+                      className="text-danger"
+                      icon="hugeicons:workflow-square-10"
+                      width={24}
+                    />
+                  }
+                  onPress={() => {
+                    router.push("/admin/flows");
+                  }}
+                >
+                  Flows
+                </DropdownItem>
+                <DropdownItem
+                  key="executions"
+                  description="Manage all executions"
+                  startContent={
+                    <Icon
+                      className="text-danger"
+                      icon="hugeicons:rocket-02"
+                      width={24}
+                    />
+                  }
+                  onPress={() => {
+                    router.push("/admin/executions");
+                  }}
+                >
+                  Executions
+                </DropdownItem>
+                <DropdownItem
+                  key="runners"
+                  description="Manage all runners"
+                  startContent={
+                    <Icon
+                      className="text-danger"
+                      icon="hugeicons:ai-brain-04"
+                      width={24}
+                    />
+                  }
+                  onPress={() => {
+                    router.push("/admin/runners");
+                  }}
+                >
+                  Runners
+                </DropdownItem>
+                <DropdownItem
+                  key="users"
+                  description="Manage all users"
+                  startContent={
+                    <Icon
+                      className="text-danger"
+                      icon="hugeicons:location-user-02"
+                      width={24}
+                    />
+                  }
+                  onPress={() => {
+                    router.push("/admin/users");
+                  }}
+                >
+                  Users
+                </DropdownItem>
+                <DropdownItem
+                  key="page_settings"
+                  description="Manage the page settings"
+                  startContent={
+                    <Icon
+                      className="text-danger"
+                      icon="hugeicons:settings-05"
+                      width={24}
+                    />
+                  }
+                  onPress={() => {
+                    router.push("/admin/page-settings");
+                  }}
+                >
+                  Page Settings
+                </DropdownItem>
+              </DropdownMenu>
+            </Dropdown>
+          )}
         </div>
       </NavbarMenu>
     </HeroUINavbar>
