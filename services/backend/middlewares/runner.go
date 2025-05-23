adc9e8afdf3fe7eaf0d4d00856e9bb46d57f3fa2
Justin Neubert <neubert_justin@outlook.com>
2025-05-23 19:42:47 +0200

    feat: add shared runner secret configuration and validation

--- services/backend/middlewares/runner.go
+++ services/backend/middlewares/runner.go
@@ -3,6 +3,7 @@ package middlewares
 import (
 	"errors"
 
+	"github.com/v1Flows/exFlow/services/backend/config"
 	"github.com/v1Flows/exFlow/services/backend/functions/auth"
 	"github.com/v1Flows/exFlow/services/backend/functions/httperror"
 
@@ -17,8 +18,20 @@ func Runner(db *bun.DB) gin.HandlerFunc {
 			httperror.Unauthorized(context, "Request does not contain an access token", errors.New("request does not contain an access token"))
 			return
 		}
+
 		err := auth.ValidateToken(tokenString)
 		if err != nil {
+			// if the token is not valid
+			// check if the token matches the config.runner.shared_runner_secret
+			if config.Config.Runner.SharedRunnerSecret != "" {
+				if tokenString != config.Config.Runner.SharedRunnerSecret {
+					httperror.Unauthorized(context, "The provided secret is not valid", err)
+					return
+				} else {
+					context.Next()
+				}
+			}
+
 			httperror.Unauthorized(context, "The provided token is not valid", err)
 			return
 		}
