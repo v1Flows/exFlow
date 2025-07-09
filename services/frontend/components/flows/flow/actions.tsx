e3750d6f9dfb6ec40a87cf8bc0e5f7da449b459d
Justin Neubert <neubert_justin@outlook.com>
2025-07-09 14:53:32 +0200

    feat: update shared-library dependency to v1.0.24 and enhance action conditions in AddActionModal

--- services/frontend/components/flows/flow/actions.tsx
+++ services/frontend/components/flows/flow/actions.tsx
@@ -583,6 +583,85 @@ export default function Actions({
                       </Table>
                     </AccordionItem>
                   )}
+                  {action.condition.selected_action_id !== "" && (
+                    <AccordionItem
+                      key={action.id + "-conditions"}
+                      aria-label="Conditions"
+                      subtitle="View condition details (click to expand)"
+                      title="Conditions"
+                    >
+                      <div className="flex items-center gap-2 mb-2">
+                        <div className="flex size-10 items-center justify-center rounded-small bg-primary/10 text-primary">
+                          <Icon
+                            icon={
+                              flow.actions.filter(
+                                (a: any) =>
+                                  a.id === action.condition.selected_action_id,
+                              )[0]?.icon
+                            }
+                            width={26}
+                          />
+                        </div>
+                        <div>
+                          <div className="flex-cols flex gap-2">
+                            <p className="text-md font-bold">
+                              {flow.actions.filter(
+                                (a: any) =>
+                                  a.id === action.condition.selected_action_id,
+                              )[0]?.custom_name ||
+                                flow.actions.filter(
+                                  (a: any) =>
+                                    a.id ===
+                                    action.condition.selected_action_id,
+                                )[0]?.name ||
+                                action.condition.selected_action_id}
+                            </p>
+                          </div>
+                          <p className="text-sm text-default-500">
+                            {flow.actions.filter(
+                              (a: any) =>
+                                a.id === action.condition.selected_action_id,
+                            )[0]?.custom_description ||
+                              flow.actions.filter(
+                                (a: any) =>
+                                  a.id === action.condition.selected_action_id,
+                              )[0]?.description ||
+                              "No description available"}
+                          </p>
+                        </div>
+                      </div>
+                      <Table
+                        removeWrapper
+                        aria-label="Details"
+                        className="w-full"
+                      >
+                        <TableHeader>
+                          <TableColumn align="center">Key</TableColumn>
+                          <TableColumn align="center">Type</TableColumn>
+                          <TableColumn align="center">Value</TableColumn>
+                          <TableColumn align="center">Logic</TableColumn>
+                        </TableHeader>
+                        <TableBody emptyContent="No patterns defined.">
+                          {action.condition.condition_items.map(
+                            (condition: any, index: number) => (
+                              <TableRow key={index}>
+                                <TableCell>{condition.condition_key}</TableCell>
+                                <TableCell>
+                                  {condition.condition_type}
+                                </TableCell>
+                                <TableCell>
+                                  {condition.condition_value}
+                                </TableCell>
+                                <TableCell>
+                                  {condition.condition_logic}
+                                </TableCell>
+                              </TableRow>
+                            ),
+                          )}
+                        </TableBody>
+                      </Table>
+                    </AccordionItem>
+                  )}
                 </Accordion>
               </div>
             </div>
