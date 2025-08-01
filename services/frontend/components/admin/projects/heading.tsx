40bb951986f6640ebf5ad2c3a48c8d12846b002e
Justin Neubert <neubert_justin@outlook.com>
2025-08-01 09:00:32 +0200

    feat: update headings and navbar descriptions to include "Admin" prefix and clarify settings

--- services/frontend/components/admin/projects/heading.tsx
+++ services/frontend/components/admin/projects/heading.tsx
@@ -12,7 +12,9 @@ export default function AdminProjectsHeading() {
     <main>
       <div className="grid grid-cols-2 items-center justify-between gap-2 lg:grid-cols-2">
         <div className="flex flex-wrap items-center gap-2">
-          <p className="text-2xl font-bold mb-1">Projects</p>
+          <p className="text-2xl font-bold mb-1">
+            <span className="text-danger">Admin</span> | Projects
+          </p>
         </div>
         <div className="flex flex-cols justify-end gap-2">
           <div className="hidden sm:flex gap-2">
--- services/frontend/components/admin/runners/heading.tsx
+++ services/frontend/components/admin/runners/heading.tsx
@@ -25,7 +25,9 @@ export default function AdminRunnersHeading({ settings }: any) {
     <main>
       <div className="grid grid-cols-2 items-center justify-between gap-2 lg:grid-cols-2">
         <div className="flex flex-wrap items-center gap-2">
-          <p className="text-2xl font-bold mb-1">Runners</p>
+          <p className="text-2xl font-bold mb-1">
+            <span className="text-danger">Admin</span> | Runners
+          </p>
         </div>
         <div className="flex flex-cols justify-end gap-2">
           <div className="hidden sm:flex gap-2">
