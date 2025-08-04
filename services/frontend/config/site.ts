47e1f2424fcc39e9948e1f67031c8cd39593913e
Justin Neubert <neubert_justin@outlook.com>
2025-08-04 11:02:34 +0200

    chore: update version to 1.4.0 in release notes, main.go, and site.ts

--- services/frontend/config/site.ts
+++ services/frontend/config/site.ts
@@ -3,7 +3,7 @@ export type SiteConfig = typeof siteConfig;
 export const siteConfig = {
   name: "exFlow",
   description: "exFlow is an workflow automation tool",
-  version: "1.3.1",
+  version: "1.4.0",
   navItems: [
     {
       label: "Dashboard",
