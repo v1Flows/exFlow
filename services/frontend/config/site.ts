3f0ab1d839a56159028e84915d6dcd479b620280
Justin Neubert <neubert_justin@outlook.com>
2025-11-15 11:06:19 +0100

    chore: Update version to 2.0.0-beta.1 in main.go and site.ts

--- services/frontend/config/site.ts
+++ services/frontend/config/site.ts
@@ -3,7 +3,7 @@ export type SiteConfig = typeof siteConfig;
 export const siteConfig = {
   name: "JustFlow",
   description: "JustFlow is an workflow automation tool",
-  version: "2.0.0",
+  version: "2.0.0-beta.1",
   navItems: [
     {
       label: "Dashboard",
