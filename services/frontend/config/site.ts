dad7a35fa439e63f39c687d0e943424477d1b4ee
Justin Neubert <neubert_justin@outlook.com>
2025-11-24 09:45:52 +0100

    chore: update version to 2.0.0-beta.3 in backend and frontend configurations

--- services/frontend/config/site.ts
+++ services/frontend/config/site.ts
@@ -3,7 +3,7 @@ export type SiteConfig = typeof siteConfig;
 export const siteConfig = {
   name: "JustFlow",
   description: "JustFlow is an workflow automation tool",
-  version: "2.0.0-beta.2",
+  version: "2.0.0-beta.3",
   navItems: [
     {
       label: "Dashboard",
